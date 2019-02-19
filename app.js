3a39187fea3a5de61a1242db7390741f9aa3aaee
Mrinmoy Chowdhury <mrinmoy.btkol@gmail.com>
2019-02-19 15:37:11 +0530

    Adding Usernames

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var nicknames = [];
+
 server.listen(3000);
 
 app.get('/', function(req, res){
@@ -12,11 +14,35 @@ app.get('/', function(req, res){
 
 // receive the event on the server side 
 io.sockets.on('connection', function(socket){
+    socket.on('new user', function(data, callback){
+        if(nicknames.indexOf(data) != -1){
+            callback(false);
+        } else{
+            callback(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+            updateNicknames();
+            
+        }
+
+    });
+
+    function updateNicknames(){
+        //console.log(nicknames);
+        io.sockets.emit('usernames', nicknames);
+    }
     socket.on('send message', function(data){
         // message send to all users
-        io.sockets.emit('new message', data);
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
         // message send to all other users except me
         //socket.broadcast.emit('new message', data);
-    })
+    });
+
+    // when close the chat window
+    socket.on('disconnect', function(data){
+        if(!socket.nickname) return;
+        nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+        updateNicknames();
+    });
 
 })
